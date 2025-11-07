e5689baa734403fa40d6d29fa0c5afa9be46d9bf
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-07 13:47:25 +0700

    konfigurasi file FormIsian.kt

--- app/src/main/java/com/example/questnavigastugas_248/view/FormIsian.kt
+++ app/src/main/java/com/example/questnavigastugas_248/view/FormIsian.kt
@@ -1,2 +1,14 @@
-package com.example.questnavigastugas_248.view
+@file:OptIn(ExperimentalMaterial3Api::class)
 
+package com.example.pertemuan6.view
+
+import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.pertemuan6.R
