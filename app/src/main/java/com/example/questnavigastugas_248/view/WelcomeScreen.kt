5ff1139d9252c2e9344ea1a5d5826afa676f1066
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-07 15:17:19 +0700

    konfigurasi WelcomeScreen.kt

--- app/src/main/java/com/example/questnavigastugas_248/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/questnavigastugas_248/view/WelcomeScreen.kt
@@ -1,2 +1,15 @@
 package com.example.questnavigastugas_248.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
