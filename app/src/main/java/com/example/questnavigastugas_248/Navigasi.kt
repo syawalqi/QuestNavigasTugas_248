67faae24ce9314e83f21768d819624aa3e1ca31c
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-07 13:46:05 +0700

    membuat minor fixes di Navigasi.kt

--- app/src/main/java/com/example/questnavigastugas_248/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas_248/Navigasi.kt
@@ -1,9 +1,15 @@
 package com.example.QuestNavigasiTugas_248
 
-
-
-
-
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.pertemuan6.view.TampilData
+import com.example.pertemuan6.view.FormIsian
 
 enum class Navigasi {
     Formulir,
